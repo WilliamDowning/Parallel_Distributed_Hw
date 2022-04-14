1a870964117e5f5ac9fc778ad76e0b743addbb03
BuildTools <unconfigured@null.spigotmc.org>
2022-04-14 14:28:20 -0400

    -This is just a test to see how the gitBash work

---
+++ src/Server.java
@@ -0,0 +1,10 @@
+/**
+ * Establish connection with host
+ * have routing table of fixed threads
+ * take in array of length
+ * implement merge sort with threads
+ * return ordered array
+ * Scenarios: 1T for N=10, 2T for N=100, 4T for N=1000, 4T for N=10000, 8T for N=10000, 16T for N=10000
+ */
+public class Server {
+}
