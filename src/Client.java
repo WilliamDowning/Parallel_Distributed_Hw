054d9dfa02ff6791a1bd4303ab2e8785737773cb
BuildTools <unconfigured@null.spigotmc.org>
2022-04-14 15:44:08 -0400

    --created basic TCP connection

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,26 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
+
+    private Socket socket;
+
+    public Client(Socket socket){
+        this.socket = socket;
+    }
+
+
+    public static void main(String[] args) {
+
+        try {
+            Socket clientSocket = new Socket("localhost", 5555);
+            Client client = new Client(clientSocket);
+
+        }catch(IOException e){
+            System.out.println("Failed to make connection to Server from Client main method");
+        }
+
+    }
 }
+
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,9 @@
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 /**
  * Establish connection with host
  * have routing table of fixed threads
@@ -7,4 +13,37 @@
  * Scenarios: 1T for N=10, 2T for N=100, 4T for N=1000, 4T for N=10000, 8T for N=10000, 16T for N=10000
  */
 public class Server {
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void start_Server(){
+
+        try{
+            System.out.println("Listening to port: 5555");
+            while(!serverSocket.isClosed()){
+               clientSocket = serverSocket.accept();
+               System.out.println("The Server has connected with its client at address: " + clientSocket.getInetAddress().getHostAddress());
+            }
+        }catch(Exception e){
+            System.out.println("Failed to establish connection to Client from Server start_server");
+
+        }
+    }
+
+    public static void main(String[] args){
+        String clientIP = "192.168.1.114";
+        try {
+            ServerSocket ss = new ServerSocket(5555);
+            Server server = new Server(ss);
+            server.start_Server();
+        }catch(IOException e){
+            System.out.println("Failed to create Server Socket Connection at Server Main method");
+
+        }
+    }
 }
